63b07527e4ad163dc1c0f4f45f53780e5b3aa714
mihirar <65051627+mihirar@users.noreply.github.com>
2023-04-12 14:48:29 -0400

    Update addPlant.js

--- pages/addPlant.js
+++ pages/addPlant.js
@@ -1,17 +1,44 @@
 import React, { useState } from 'react';
-import { Image, Text, View, TextInput} from 'react-native';
+import { Image, Text, View, TextInput, ScrollView, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 import styles from '../styles/addPlantStyle';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {firebase} from '../firebase';
+import { firebase } from '../firebase';
+
+const PLANT_ID_API_KEY = 'L4rTSXH7FplvKiyHzWsjWe5hWcfpvEK5JhMYCIeuKJscw5kGeS';
+
+async function detectPlant(imageUri) {
+  const base64Image = await FileSystem.readAsStringAsync(imageUri, {
+    encoding: FileSystem.EncodingType.Base64,
+  });
+
+  const body = JSON.stringify({
+    organs: ['leaf', 'flower', 'fruit', 'bark', 'habit'],
+    api_key: PLANT_ID_API_KEY,
+    images: [base64Image],
+    organelle: 'plant',
+  });
+
+  const response = await fetch('https://api.plant.id/v2/identify', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body,
+  });
+
+  const json = await response.json();
+  return json;
+}
 
 export default function AddPlant() {
   const [imageUri, setImageUri] = useState(null);
   const [imageBlob, setImageBlob] = useState(null);
-  const [plantName, setPlantName] = useState("");
-  const [plantSpecies, setPlantSpecies] = useState("");
-  const [plantDescription, setPlantDescription] = useState("");
+  const [plantName, setPlantName] = useState('');
+  const [plantSpecies, setPlantSpecies] = useState('');
+  const [plantDescription, setPlantDescription] = useState('');
 
   const selectImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -22,35 +49,43 @@ export default function AddPlant() {
     if (!result.cancelled) {
       setImageUri(result.uri);
       setImageBlob(result);
+
+      const plantData = await detectPlant(result.uri);
+      if (plantData && plantData.suggestions && plantData.suggestions.length > 0) {
+        setPlantSpecies(plantData.suggestions[0].plant_name);
+      } else {
+        Alert.alert('Error', 'Plant not identified');
+      }
     }
   };
-  const uploadImage = async() =>{
-    if(!imageBlob) return;
+
+  const uploadImage = async () => {
+    if (!imageBlob) return;
 
     const response = await fetch(imageBlob.uri);
     const blob = await response.blob();
-    const imageName = 'images/${Math.random().toString(36).substring(7)}.jpg';
-    
-    const storageRef = firebase.storage().storageRef().child(imageName);
-    await storageReg.put(blob);
+    const imageName = `images/${Math.random().toString(36).substring(7)}.jpg`;
+
+    const storageRef = firebase.storage().ref().child(imageName);
+    await storageRef.put(blob);
     const downloadUrl = await storageRef.getDownloadURL();
-    console.log("before image upload");
-    console.log("Image uploaded: ", downloadUrl);
-  }
-  const handleAddPlant = () =>{
+    console.log('before image upload');
+    console.log('Image uploaded: ', downloadUrl);
+  };
+
+  const handleAddPlant = () => {
     uploadImage();
-  }
-  
+  };
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.pageBox}>
         <View style={styles.imageContainer}>
-          <TouchableOpacity onPress={selectImage} style={[styles.uploadArea, imageUri && {opacity: 0}]}>
+          <TouchableOpacity onPress={selectImage} style={[styles.uploadArea, imageUri && { opacity: 0 }]}>
             <View style={styles.imageDottedBorder}>
               {imageUri && <Image source={{ uri: imageUri }} style={styles.image} resizeMode="cover" />}
-              <Icon name='arrow-up'style={[imageUri && {display: 'none'}]} size={50} color="#385250"></Icon>
-              <Text style={[styles.uploadText, imageUri && {display: 'none'}]}>Upload</Text>
+              <Icon name='arrow-up' style={[imageUri && { display: 'none' }]} size={50} color="#385250"></Icon>
+              <Text style={[styles.uploadText, imageUri && { display: 'none' }]}>Upload</Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -59,7 +94,7 @@ export default function AddPlant() {
             <TextInput
               placeholder='Give your plant a name'
               placeholderTextColor="#385250"
-              style={[styles.plantNameInput, {fontSize: 26,fontFamily: 'Kabel-Black'}]}
+              style={[styles.plantNameInput, { fontSize: 26, fontFamily: 'Kabel-Black' }]}
               onChangeText={setPlantName}
               value={plantName}
             >
@@ -69,7 +104,7 @@ export default function AddPlant() {
             <TextInput
               placeholder="Plant Species"
               placeholderTextColor="#385250"
-              style={[styles.plantSpeciesInput, {fontFamily: "JosefinSans", fontSize: 20,fontWeight: 'bold'}]}
+              style={[styles.plantSpeciesInput, { fontFamily: "JosefinSans", fontSize: 20, fontWeight: 'bold' }]}
               onChangeText={setPlantSpecies}
               value={plantSpecies}
             />
@@ -81,18 +116,19 @@ export default function AddPlant() {
               textAlignVertical="top"
               placeholder="Add a description..."
               placeholderTextColor="#385250"
-              style={[styles.plantDescriptionInput, {fontFamily: "JosefinSans", fontSize: 20,fontWeight: 'bold',}]}
-              onChangeText={text =>setPlantDescription(text)}
+              style={[styles.plantDescriptionInput, { fontFamily: "JosefinSans", fontSize: 20, fontWeight: 'bold' }]}
+              onChangeText={text => setPlantDescription(text)}
               value={plantDescription}
             />
           </View>
-          </View>
-          <View style={styles.confirmArea}>
-            <TouchableOpacity onPress={handleAddPlant} style={styles.saveBtn}>
-              <Text style={{fontSize: 22,fontFamily: 'Kabel-Black' ,color: '#FFFFFF',}}>Save</Text>
-            </TouchableOpacity>
-          </View>
+        </View>
+        <View style={styles.confirmArea}>
+          <TouchableOpacity onPress={handleAddPlant} style={styles.saveBtn}>
+            <Text style={{ fontSize: 22, fontFamily: 'Kabel-Black', color: '#FFFFFF' }}>Save</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
+
